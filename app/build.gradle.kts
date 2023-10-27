92252e2de62e45ce732ab01de88e9a283afaafcd
NesSalgado <corporativoteprotejo@gmail.com>
2023-10-26 22:51:08 -0500

    esta es la pantalla correcta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "olave.hans.ecoreciclagroup"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "olave.hans.ecoreciclagroup"
